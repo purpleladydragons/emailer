a1c4254b4030fae1ec37e74abb3d67d8b641c03b
Austin Steady <asteady23@gmail.com>
2021-11-21 20:26:19 -0800

    emailer app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Gmail Emailer',
+      version='0.1',
+      description='Send emails from gmail account',
+      author='Austin Steady',
+      author_email='asteady23@gmail.com',
+      url='https://github.com/purpleladydragons',
+      packages=['emailer'],
+     )
